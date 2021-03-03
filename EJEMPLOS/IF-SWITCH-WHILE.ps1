d82b5c920695fbbcd5609d5ae2d4b3c1dd433e03
Ismael Morilla <79579288+cinqueles@users.noreply.github.com>
2021-03-03 15:19:41 +0100

    Ejemplos simples
    Carpeta con ejemplos de script simples, para entender mejor el funcionamiento.

---
+++ EJEMPLOS/IF-SWITCH-WHILE.ps1
@@ -0,0 +1,42 @@
+﻿<#
+SCRIPT DE EJEMPLO
+AUTOR: ISMAEL MORILLA
+FECHA: 3/2/2021
+DESCRIPCIÓN: EJEMPLO DE ESTRUCTRUAS IF, SWITCH Y WHILE.
+#>
+
+
+
+# PRUEBA IF
+Clear-Host
+[int]$opcion= Read-Host 'elige opcion {1-5}'
+
+if ( $opcion -le 5 ) {
+    #SWITCH PARA OPCIONES DEL MENÚ
+    switch ( $opcion ) {
+        1 {"hola"}
+        2 {"adios"}
+        3 {"hola"}
+        4 {"adios"}
+        5 {"hola"}
+    }
+
+}else{
+    while ( $opcion -gt 5 ) {
+        Write-Host 'incorrecto'
+        [int]$opcion= Read-Host 'elige opcion {1-5}'
+
+        # para ver si es menor o igual que 5
+        # SWTICH PARA OPCIONES DEL MENÚ
+        if  ( $opcion -le 5 ) {
+            switch ( $opcion ) {
+                1 {"hola"}
+                2 {"adios"}
+                3 {"hola"}
+                4 {"adios"}
+                5 {"hola"}
+            }
+        }
+    }
+
+}
---
+++ EJEMPLOS/puertos.ps1
@@ -0,0 +1,22 @@
+﻿<#
+SCRIPT DE EJEMPLO
+AUTOR: ISMAEL MORILLA
+FECHA: 3/2/2021
+DESCRIPCIÓN: SCRIPT QUE TE PIDE UN PUERTO LOCAL IDENTIFICANDO EL PROCESO AL QUE PERTENECE.
+#>
+
+Clear-Host
+
+#MOSTRAR LOS PUERTOS
+Get-NetTCPConnection | ft LocalPort, State 
+
+#Petición del puerto a identificar
+$localp = Read-Host 'Indique el puerto local que quiere identificar:' 
+
+#Identificación de la ID del proceso propietario
+$id = (Get-NetTCPConnection | Where-Object {$_.LocalPort -eq $localp}).OwningProcess | Get-Unique
+
+#Identificación del proceso
+$proces =(Get-Process | Where-Object {$_.Id -eq $id})
+
+Write-Host 'El puerto ' $localp ' está asignado al servicio/proceso' $proces
